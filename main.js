67225c3720056fe44619ee8135e03568f3a493a9
MotorTruck1221 <tuckerj0606@icloud.com>
2023-04-08 00:05:58 -0600

    Switch to electron forge. NOTE: break CI

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-const setupEvents = require('./installers/setupEvents')
- if (setupEvents.handleSquirrelEvent()) {
-    // squirrel event handled and app will exit in 1000ms, so don't do anything else
-    return;
- }
 const electron = require('electron');
 const path = require('path');
 const { app, BrowserWindow, Menu, ipcMain } = electron;
+if (require('electron-squirrel-startup')) app.quit();
 const Store = require('./settings.js');
 
 const settings = new Store({
