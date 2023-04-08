a0b8d9d1e8447d82db9a5a74b7ff6f7178325c00
Allan Niles <allancoding.dev@gmail.com>
2023-04-08 10:16:17 -0600

    add new things!

--- README.md
+++ README.md
@@ -13,7 +13,5 @@ npm i
 npm start
 ```
 
-To run the Demo (btd5), enter the url as `file:///home/<USERNAME>/Downloads/flashplayer-main /demo/btd5test.html`
-Note: Only works in a dev environment.
 
 <sup>This is preinstalled in [PrismOS](https://github.com/PrismNet)</sub>
--- forge.config.js
+++ forge.config.js
@@ -1,24 +1,58 @@
 module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
-  makers: [
+  publishers: [
     {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'PrismNet',
+          name: 'flashplayer'
+        },
+        prerelease: false
+      }
+    }
+  ],
+  makers: [{
       name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
+      config: {
+        authors: 'PrismNet',
+        description: 'A flash player!',
+        //certificateFile: './cert.pfx',
+        //certificatePassword: process.env.CERTIFICATE_PASSWORD
+      },
+    },{
       name: '@electron-forge/maker-zip',
       platforms: ['darwin', 'linux'],
-    },
-    {
+    },{
       name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
+      config: {
+        options: {
+          maintainer: 'PrismNet',
+          homepage: 'https://github.com/PrismNet/flashplayer'
+        }
+      },
+    },{
+      name: '@electron-forge/maker-dmg',
+      config: {
+        //background: './assets/dmg-background.png',
+        format: 'ULFO'
+      }
+    },{
       name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-    { name: '@electron-forge/maker-pkg', config: {}},
-    { name: '@rabbitholesyndrome/electron-forge-maker-portable' }
+      config: {
+        options: {
+          maintainer: 'PrismNet',
+          homepage: 'https://github.com/PrismNet/flashplayer'
+        }
+      }
+    },{
+      name: "@rabbitholesyndrome/electron-forge-maker-portable",
+      config: {
+        appId: "net.prism.flashplayer"
+      }
+    },{ name: '@electron-forge/maker-pkg', config: {
+      //keychain: 'my-secret-ci-keychain'
+    }},{ name: 'electron-forge-maker-appimage' }
   ],
 };
