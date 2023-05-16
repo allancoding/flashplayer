ee3f31ea4b1190315835b7cc57dca627148be019
Allan Niles <admin@allancoding.ga>
2023-05-16 08:08:34 -0600

    half update

--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    setUrl: (url, type) => ipcRenderer.send('setUrl', url, type)
+    setUrl: (url, type) => ipcRenderer.send('setUrl', url, type),
+    fullscreen: (func) => {
+        ipcRenderer.on("fullscreen", (event, ...args) => func(...args));
+    }
 })
