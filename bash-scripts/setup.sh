bc0534a65b9e49f3633a7c4b1b46065d587aec52
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-22 03:42:38 -0600

    Cool this SHOULD work

--- bash-scripts/setup.sh
+++ bash-scripts/setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+# Detect if the tag has -beta in it
+# If it does this command will be slightly different
+
+if [[ $tag == *"beta"* ]]; then
+    gh release create "$tag" \
+        --repo="$GITHUB_REPOSITORY" \
+        --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
+        --generate-notes \ 
+        --prerelease
+else
+    gh release create "$tag" \
+        --repo="$GITHUB_REPOSITORY" \
+        --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
+        --generate-notes \ 
+fi
+
+
 current_version=$(cat package.json | grep version | cut -d '"' -f 4)
 echo "Current version is $current_version"
 sed -i "s/$current_version/$tag/g" package.json
 git add .
-#setup git user (with github workflow)
 git config --global user.email "motortruck1221@protonmail.com"
-git config --global user.name "motortruck1221"
+git config --global user.name "GitHub Actions"
 git commit -m "Bump version to $tag"
 git push -u origin main
