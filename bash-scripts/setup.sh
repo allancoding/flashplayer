7547dc19501b641c00cf7a1597702e27d21302e9
MotorTruck1221 <73721704+MotorTruck1221@users.noreply.github.com>
2023-05-22 08:02:19 -0600

    Update setup.sh

--- bash-scripts/setup.sh
+++ bash-scripts/setup.sh
@@ -12,15 +12,5 @@ else
     gh release create "$tag" \
         --repo="$GITHUB_REPOSITORY" \
         --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
-        --generate-notes \ 
-fi
-
-
-current_version=$(cat package.json | grep version | cut -d '"' -f 4)
-echo "Current version is $current_version"
-sed -i "s/$current_version/$tag/g" package.json
-git add .
-git config --global user.email "motortruck1221@protonmail.com"
-git config --global user.name "GitHub Actions"
-git commit -m "Bump version to $tag"
-git push -u origin main
+        --generate-notes \
+fi  
