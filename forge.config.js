65e421af898235ff94bd5f100e90b3ada124fb57
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-22 03:44:20 -0600

    SWEET

--- forge.config.js
+++ forge.config.js
@@ -16,7 +16,6 @@ module.exports = {
            owner: 'allancoding',
            name: 'flashplayer'
          },
-         prerelease: true,
          authToken: process.env.GITHUB_TOKEN
        }
      }
