e9fb69b342d42418c87bc23434262dff469b4885
MotorTruck1221 <73721704+MotorTruck1221@users.noreply.github.com>
2023-05-22 08:07:02 -0600

    Update setup.sh

--- bash-scripts/setup.sh
+++ bash-scripts/setup.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-# Detect if the tag has -beta in it
-# If it does this command will be slightly different
+# # Detect if the tag has -beta in it
+# # If it does this command will be slightly different
 
-if [[ $tag == *"beta"* ]]; then
-    gh release create "$tag" \
+# if [[ $tag == *"beta"* ]]; then
+#     gh release create "$tag" \
+#         --repo="$GITHUB_REPOSITORY" \
+#         --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
+#         --generate-notes \ 
+#         --prerelease
+# else
+#     gh release create "$tag" \
+#         --repo="$GITHUB_REPOSITORY" \
+#         --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
+#         --generate-notes \
+# fi
+     gh release create "$tag" \
         --repo="$GITHUB_REPOSITORY" \
         --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
         --generate-notes \ 
         --prerelease
-else
-    gh release create "$tag" \
-        --repo="$GITHUB_REPOSITORY" \
-        --title="${GITHUB_REPOSITORY#*/} ${tag#v}" \
-        --generate-notes \
-fi  
