ef940ac8a1d11e88264cf62753b57488e3c1f4b0
MotorTruck1221 <tuckerj0606@icloud.com>
2023-05-22 03:34:47 -0600

    hmm

--- bash-scripts/setup.sh
+++ bash-scripts/setup.sh
@@ -2,6 +2,9 @@
 current_version=$(cat package.json | grep version | cut -d '"' -f 4)
 echo "Current version is $current_version"
 sed -i "s/$current_version/$tag/g" package.json
-git add package.json
-git commit -m "Updated version number to $tag"
-git push
+git add .
+#setup git user (with github workflow)
+git config --global user.email "motortruck1221@protonmail.com"
+git config --global user.name "motortruck1221"
+git commit -m "Bump version to $tag"
+git push origin master
